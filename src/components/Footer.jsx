b7aab02d3c6a02238767a44545a6ef7710cc3d6f
LuisRGN <luisrafael1598@gmail.com>
2024-07-02 18:03:55 -0500

    se cambia el link de linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="text-white py-4 flex flex-col justify-center items-center">
+    <footer className="flex flex-col items-center justify-center py-4 text-white">
       <div className="container mx-auto text-center">
         &copy; 2024 Luis Gonzalez. Todos los derechos reservado.
       </div>
       <div className='flex items-center'>
-        <a href="https://github.com/LuisRGN" target="_blank" rel="noopener noreferrer"><img className='h-8 mr-1 ml-1 hover:opacity-75' src="/githubWhite.svg" alt="" /></a>
-        <a href="https://www.linkedin.com/in/luis-gonzalez-nu%C3%B1ez-049076290/" target="_blank" rel="noopener noreferrer"><img className='h-10 hover:opacity-75' src="/linkedin.svg" alt="" /></a>
+        <a href="https://github.com/LuisRGN" target="_blank" rel="noopener noreferrer"><img className='h-8 ml-1 mr-1 hover:opacity-75' src="/githubWhite.svg" alt="" /></a>
+        <a href="https://www.linkedin.com/in/luisrgn/" target="_blank" rel="noopener noreferrer"><img className='h-10 hover:opacity-75' src="/linkedin.svg" alt="" /></a>
       </div>
     </footer>
   );
