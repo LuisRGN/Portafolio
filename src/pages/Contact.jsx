e1eebf82b1b7a7e245176535ef7e4565c337aa56
LuisRGN <luisrafael1598@gmail.com>
2024-07-10 21:50:45 -0500

    se agregan 3 props para el color del fondo, para rel color del textop y para el color del boton

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,14 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
 import emailjs from 'emailjs-com';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
-const Contact = () => {
+const Contact = ({ aboutBgClass, colorText, btnColorClass }) => {
   const [input, setInput] = useState({
-    user_name: "",
-    user_email: "",
-    message: ""
-  })
+    user_name: '',
+    user_email: '',
+    message: ''
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -23,46 +23,52 @@ const Contact = () => {
 
     if (!input.user_name || !input.user_email || !input.message) {
       Swal.fire({
-        title: "Por favor completa todos los campos",
-        icon: "warning"
+        title: 'Por favor completa todos los campos',
+        icon: 'warning'
       });
       return;
     }
 
-    emailjs.sendForm('service_rwnuc7n', 'template_nndwhip', e.target, 'i_ZUgVzWhlmZOn9Kg')
-      .then((result) => {
-        console.log(result.text);
-        Swal.fire({
-          title: "Correo enviado exitosamente",
-          icon: "success"
-        })
-      }, (error) => {
-        console.log(error.text);
-        Swal.fire({
-          title: "Error al enviar el mensaje. Por favor, inténtelo de nuevo más tarde.",
-          icon: "error"
-        })
-      });
+    emailjs
+      .sendForm('service_rwnuc7n', 'template_nndwhip', e.target, 'i_ZUgVzWhlmZOn9Kg')
+      .then(
+        (result) => {
+          console.log(result.text);
+          Swal.fire({
+            title: 'Correo enviado exitosamente',
+            icon: 'success'
+          });
+        },
+        (error) => {
+          console.log(error.text);
+          Swal.fire({
+            title: 'Error al enviar el mensaje. Por favor, inténtelo de nuevo más tarde.',
+            icon: 'error'
+          });
+        }
+      );
 
     e.target.reset();
 
     setInput({
-      user_name: "",
-      user_email: "",
-      message: ""
-    })
+      user_name: '',
+      user_email: '',
+      message: ''
+    });
   };
 
   return (
-    <section id="contact" className="flex items-center md:justify-evenly min-h-screen bg-[#c2c2c2d4]">
-      <div className='p-8 max-[768px]:hidden'>
+    <section id="contact" className={`${aboutBgClass} flex items-center justify-center md:justify-evenly min-h-screen`}>
+      <div className="p-8 max-[768px]:hidden">
         <img src="/contact.svg" alt="" />
       </div>
       <div className="p-8 w-[600px]">
-        <h2 className="text-4xl font-bold text-center">Contacto</h2>
+        <h2 className={`text-4xl font-bold text-center ${colorText}`}>Contacto</h2>
         <form className="max-w-lg mt-8" onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block mb-2 text-lg font-semibold" htmlFor="user_name">Nombre</label>
+            <label className={`block mb-2 text-lg font-semibold ${colorText}`} htmlFor="user_name">
+              Nombre
+            </label>
             <input
               className="w-full p-2 border rounded"
               type="text"
@@ -74,7 +80,9 @@ const Contact = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2 text-lg font-semibold" htmlFor="user_email">Correo</label>
+            <label className={`block mb-2 text-lg font-semibold ${colorText}`} htmlFor="user_email">
+              Correo
+            </label>
             <input
               className="w-full p-2 border rounded"
               type="email"
@@ -86,7 +94,9 @@ const Contact = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2 text-lg font-semibold" htmlFor="message">Mensaje</label>
+            <label className={`block mb-2 text-lg font-semibold ${colorText}`} htmlFor="message">
+              Mensaje
+            </label>
             <textarea
               className="w-full p-2 border rounded"
               id="message"
@@ -97,7 +107,12 @@ const Contact = () => {
               onChange={handleChange}
             ></textarea>
           </div>
-          <button className="px-4 py-2 text-white rounded bg-[#242223] hover:bg-[#1a25c2] float-right" type="submit">Enviar</button>
+          <button
+            className={`px-4 py-2 text-white rounded transition duration-300 ${btnColorClass} hover:bg-[#1a25c2] float-right`}
+            type="submit"
+          >
+            Enviar
+          </button>
         </form>
       </div>
     </section>
@@ -105,4 +120,5 @@ const Contact = () => {
 };
 
 export default Contact;
+
 
