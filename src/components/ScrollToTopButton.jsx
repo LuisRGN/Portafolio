72ce375a842c0029a011738de82776691e36d2cb
LuisRGN <luisrafael1598@gmail.com>
2024-07-10 21:49:30 -0500

    se agrega un prop para el color del boton

--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
-const ScrollToTopButton = () => {
+const ScrollToTopButton = ({ btnColorClass }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
@@ -34,7 +35,7 @@ const ScrollToTopButton = () => {
       {isVisible && (
         <button
           onClick={scrollToTop}
-          className="p-3 text-white transition duration-300 bg-[#242223] rounded-full shadow-lg hover:bg-[#1a25c2]"
+          className={`p-3 text-white transition duration-300 ${btnColorClass} rounded-full shadow-lg hover:bg-[#1a25c2]`}
           style={{ width: '60px', height: '60px' }}
         >
           <FontAwesomeIcon icon={faArrowUp} size="lg" />
