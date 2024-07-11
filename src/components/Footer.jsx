01b1166c438ab361db2412dc1fdea69644e8c2cd
LuisRGN <luisrafael1598@gmail.com>
2024-07-10 21:48:12 -0500

    se agrega el prop para cambiar el color del fondo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,16 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-const Footer = () => {
+const Footer = ({ bgClass }) => {
   return (
-    <footer className="flex flex-col items-center justify-center py-4 text-white">
+    <footer className={`flex flex-col items-center justify-center py-4 ${bgClass} text-white`}>
       <div className="container mx-auto text-center">
-        &copy; 2024 Luis Gonzalez. Todos los derechos reservado.
+        &copy; 2024 Luis Gonzalez. Todos los derechos reservados.
       </div>
       <div className='flex items-center'>
-        <a href="https://github.com/LuisRGN" target="_blank" rel="noopener noreferrer"><img className='h-8 ml-1 mr-1 hover:opacity-75' src="/githubWhite.svg" alt="" /></a>
-        <a href="https://www.linkedin.com/in/luisrgn/" target="_blank" rel="noopener noreferrer"><img className='h-10 hover:opacity-75' src="/linkedin.svg" alt="" /></a>
+        <a href="https://github.com/LuisRGN" target="_blank" rel="noopener noreferrer"><img className='h-8 ml-1 mr-1 hover:opacity-75' src="/githubWhite.svg" alt="GitHub" /></a>
+        <a href="https://www.linkedin.com/in/luisrgn/" target="_blank" rel="noopener noreferrer"><img className='h-10 hover:opacity-75' src="/linkedin2.svg" alt="LinkedIn" /></a>
       </div>
     </footer>
   );
