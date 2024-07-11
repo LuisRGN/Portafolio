b3e75b96f9f5e99a9287726992f58419e9b6ed91
LuisRGN <luisrafael1598@gmail.com>
2024-07-10 21:51:07 -0500

    se agregan 2 props pare el color del fondo y el color del boton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-const Home = () => {
+const Home = ({ bgColorClass, colorText }) => {
   return (
-    <section id="home" className='min-h-screen bg-[#fff] flex items-center justify-center flex-col'>
+    <section id="home" className={`min-h-screen flex items-center justify-center flex-col ${bgColorClass}`}>
       <div className="flex items-center justify-center">
         <div>
-          <img src="/developer.svg" alt="" className='md:h-[15rem] h-[80%]' />
+          <img src="/developer.svg" alt="" className="md:h-[15rem] h-[80%]" />
         </div>
       </div>
-      <div className='text-center max-w-[80%] mt-[4rem]'>
-        <h1 className="text-5xl font-bold">Bienvenido a mi portafolio</h1>
-        <p className="mt-4 text-xl">Soy un desarrollador web apasionado por crear sitios web atractivos y funcionales.</p>
+      <div className="text-center max-w-[80%] mt-[4rem]">
+        <h1 className={`text-5xl font-bold ${colorText}`}>Bienvenido a mi portafolio</h1>
+        <p className={`mt-4 text-xl ${colorText}`}>Soy un desarrollador web apasionado por crear sitios web atractivos y funcionales.</p>
       </div>
     </section>
   );
