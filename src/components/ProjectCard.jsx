a6209ad47632fbb0527c0c5efeb3edd379565cc0
LuisRGN <luisrafael1598@gmail.com>
2024-07-10 21:49:12 -0500

    se agrega props para el color de las letras y de los botones

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,13 +2,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-const ProjectCard = ({ title, description, link, img }) => {
+const ProjectCard = ({ title, description, link, img, btnColorClass, colorText }) => {
   return (
     <div className="p-4 border border-[#707070] rounded-lg">
-      <h3 className="text-2xl font-bold">{title}</h3>
-      <p className="mt-2">{description}</p>
+      <h3 className={`text-2xl font-bold ${colorText}`}>{title}</h3>
+      <p className={`mt-2 ${colorText}`}>{description}</p>
       <img className="my-2 rounded-sm" src={img} alt="Proyectos" />
-      <a href={link} target="_blank" className="inline-block px-4 py-2 text-white rounded bg-[#242223] hover:bg-[#1a25c2]">Ver proyecto</a>
+      <a href={link} target="_blank" className={`inline-block px-4 py-2 text-white transition duration-300 rounded ${btnColorClass} hover:bg-[#1a25c2]`}>Ver proyecto</a>
     </div>
   );
 };
