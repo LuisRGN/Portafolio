4e40ddda69632f5c23a1ac6ec5f2068a75a9bcf1
LuisRGN <luisrafael1598@gmail.com>
2024-07-15 11:38:30 -0500

    se agrean transiciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,15 @@ import React from 'react';
 
 const Home = ({ bgColorClass, colorText }) => {
   return (
-    <section id="home" className={`min-h-screen flex items-center justify-center flex-col ${bgColorClass}`}>
+    <section id="home" className={`min-h-screen flex items-center justify-center transition duration-500 flex-col ${bgColorClass}`}>
       <div className="flex items-center justify-center">
         <div>
           <img src="/developer.svg" alt="" className="md:h-[15rem] h-[80%]" />
         </div>
       </div>
       <div className="text-center max-w-[80%] mt-[4rem]">
-        <h1 className={`text-5xl font-bold ${colorText}`}>Bienvenido a mi portafolio</h1>
-        <p className={`mt-4 text-xl ${colorText}`}>Soy un desarrollador web apasionado por crear sitios web atractivos y funcionales.</p>
+        <h1 className={`text-5xl font-bold transition duration-500 ${colorText}`}>Bienvenido a mi portafolio</h1>
+        <p className={`mt-4 text-xl transition duration-500 ${colorText}`}>Soy un desarrollador web apasionado por crear sitios web atractivos y funcionales.</p>
       </div>
     </section>
   );
