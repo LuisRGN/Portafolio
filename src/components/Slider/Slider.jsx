640260255ff8b771e11c91f82300424745feaa52
LuisRGN <luisrafael1598@gmail.com>
2024-08-21 21:26:15 -0500

    se hace mas escalable el slider de imagenes

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,38 +1,41 @@
 /* eslint-disable react/prop-types */
 import styles from "./Slider.module.css"
 
+const technologies = [
+    { name: "JavaScript", img: "/javascript.svg" },
+    { name: "CSS3", img: "/css.svg" },
+    { name: "HTML5", img: "/html.svg" },
+    { name: "NODE.js", img: "/node.svg" },
+    { name: "TypeORM", img: "/typeorm.svg" },
+    { name: "Next.js", img: "/next.svg" },
+    { name: "PostgreSQL", img: "/postgresql.svg" },
+    { name: "React.js", img: "/react.svg" },
+    { name: "TailwindCSS", img: "/tailwind.svg" },
+    { name: "TypeScript", img: "/type.svg" },
+    { name: "Express.js", img: "/express.svg" },
+    { name: "GitHub", img: "/github.svg" },
+    { name: "Vercel", img: "/vercel.svg" },
+    { name: "Vue.js", img: "/vue.svg" },
+];
+
 const Slice = ({ colorText }) => {
     return (
         <div className={styles.div}>
             <div className={styles.container}>
-                <h1 className={`${colorText}`}>TECNOLOGÍAS</h1>
+                <h1 className={colorText}>TECNOLOGÍAS</h1>
                 <div className={styles.inner}>
-                    <div className={styles.tag}><img src="/javascript.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/css.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/html.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/node.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/typeorm.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/next.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/postgresql.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/react.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/tailwind.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/type.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/express.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/github.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/vercel.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/javascript.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/css.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/html.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/node.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/typeorm.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/next.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/postgresql.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/react.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/tailwind.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/type.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/express.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/github.svg" alt="" /></div>
-                    <div className={styles.tag}><img src="/vercel.svg" alt="" /></div>
+                    {technologies.map((tech, index) => (
+                        <div className={styles.tag} key={index}>
+                            <img src={tech.img} alt={tech.name} />
+                            <span className={styles.name}>{tech.name}</span>
+                        </div>
+                    ))}
+                    {technologies.map((tech, index) => (
+                        <div className={styles.tag} key={index}>
+                            <img src={tech.img} alt={tech.name} />
+                            <span className={styles.name}>{tech.name}</span>
+                        </div>
+                    ))}
                     <div className={styles.fade}></div>
                 </div>
             </div>
@@ -40,4 +43,4 @@ const Slice = ({ colorText }) => {
     )
 }
 
-export default Slice
+export default Slice;
